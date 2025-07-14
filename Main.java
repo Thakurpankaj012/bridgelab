9e13ea386420e63e41b58502497e2e770b522eb7
Thakurpankaj012 <121154319+Thakurpankaj012@users.noreply.github.com>
2025-07-14 11:05:27 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Name:Pankaj Thakur");
+        System.out.println("Roll NO: 2210992017");
+        System.out.println("Branch:CSE");
+
+    }
+}
