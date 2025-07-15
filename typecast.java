6b01bd3d664e7fd5e7204a8f1c93c9579af5dbd4
Thakurpankaj012 <121154319+Thakurpankaj012@users.noreply.github.com>
2025-07-15 12:20:50 +0530

    Add files via upload

---
+++ typecast.java
@@ -0,0 +1,10 @@
+public class typecast {
+    public static void main(String[] args) {
+        double num1=10.0;
+        int num2=(int)num1;
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+
+
+}
