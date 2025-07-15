98b6f5610031eba1bb59a6c15d441016fbcb44e4
Thakurpankaj012 <121154319+Thakurpankaj012@users.noreply.github.com>
2025-07-15 12:20:33 +0530

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class day2 {
+    public static void main(String[] args) {
+        Scanner abc = new Scanner(System.in);
+        System.out.println("enter the name ");
+        String name= abc.nextLine();
+        System.out.println("your name is " +name);
+        int num1=10;
+        String num2="20";
+        System.out.println(num1+num2);
+
+    }
+
+}
